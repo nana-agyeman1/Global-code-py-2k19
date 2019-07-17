1544d158d9d50682b0cca838201d1e18bff04b5f
nana-agyeman1 <alexagyeman98@gmail.com>
2019-07-17 15:52:31 +0000

    started web

--- gwebhook.py
+++ gwebhook.py
@@ -1,10 +1,27 @@
 import requests
 
 #url = "https://maker.ifttt.com/use/oLXaSVTrxOexMIOzthhsPukbi6F0jhWapcN3gOyCw1X"
-r = requests.post("https://maker.ifttt.com/trigger/GLBLCD/with/key/oLXaSVTrxOexMIOzthhsPukbi6F0jhWapcN3gOyCw1X")
-
+r = requests.get("https://maker.ifttt.com/trigger/HTU/with/key/oLXaSVTrxOexMIOzthhsPukbi6F0jhWapcN3gOyCw1X", {"Value1": "Hello team!"})
+#r = requests.post("https://maker.ifttt.com/trigger/HTU/with/key/oLXaSVTrxOexMIOzthhsPukbi6F0jhWapcN3gOyCw1X")
 c = r.status_code
 print(c)
+
+
+#https://maker.ifttt.com/trigger/GLBLCD/with/key/oLXaSVTrxOexMIOzthhsPukbi6F0jhWapcN3gOyCw1X
+'''Sending a message to another phone'''
+name = "Alex"
+messenger = "What's up with you"
+
+requests.get("https://maker.ifttt.com/trigger/HTU/with/key/oLXaSVTrxOexMIOzthhsPukbi6F0jhWapcN3gOyCw1X",{"Value1": "Hello team!"})
+
+
+
+
+
+
+
+
+
 
 '''import os, sys
 import RPi.GPIO as GPIO
